650c2d39a7c8504681f76d60dfdb2579b3256fda
dalpo0 <decoderhacker96@gmail.com>
2025-10-02 14:48:08 +0530

    Create init.py

---
+++ bot/init.py
@@ -0,0 +1,6 @@
+from .main import CustomizableBot
+from .database import BotDatabase
+from .customization import CustomizationSystem
+from .rank_system import RankSystem
+
+__all__ = ['CustomizableBot', 'BotDatabase', 'CustomizationSystem', 'RankSystem']
